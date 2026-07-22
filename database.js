7b385aab47b6a1341ccf63660b36978892602a54
Steffi <25034408@myrp.edu.sg>
2026-07-22 15:33:54 +0800

    Revert "Design"
    This reverts commit 5afcdf8af47f30e1a858e90d51c5d0e27f3c0d8d.

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
     host: 'c237-adib-mysql.mysql.database.azure.com',
     user: 'c237_026',
     password: 'c237026@2026!',
-    database: 'c237_026_team5_ca2',
+    database: 'c237_026_team3_ca2',
     ssl: {
         rejectUnauthorized: false
     }
